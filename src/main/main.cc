b6cffe1cc45d6b0ff6b84492575c8f04f4956c0f
anujp10 <aspatil@usc.edu>
2020-02-15 10:57:06 -0800

    Problem_4

---
+++ src/main/main.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "src/lib/solution.h"
+
+int main()
+{
+    std::vector<int> input = {10, 20, 2, 0, -8, -6, 41, -9, 5, 15, 4, 7, 22};
+    BST *tree = new BST(input);
+    std::vector <int> output = tree->LevelOrder();
+    for (auto &n : output)
+        std::cout << n << " ";
+    std::cout << std::endl;
+    return EXIT_SUCCESS;
+}
